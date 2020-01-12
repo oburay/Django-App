963b27e46b562bdbff5943d6d9af622327503b14
Obu Raymond <oburay13@gmail.com>
2020-01-12 11:20:11 +0000

    Addition of databes model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Pages(models.Model):
+    text = models.TextField()
+    #timne = models.TimeField()
+    #response = models.CharField()
+
+        
+    def __str__(self): 
+        return self.text
--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Pages
 
 # Create your views here.
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Pages
     template_name = "home.html"
 
 class AboutPageView(TemplateView):
